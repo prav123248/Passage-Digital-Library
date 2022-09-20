2e8a3e5876088b368a161200fdfd6a2fe9c87e49
Pravin Prabhakaran <78224090+prav123248@users.noreply.github.com>
2022-09-20 16:21:40 +0100

    partial integration with django

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './static/App.css';
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col, Navbar, Nav } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
