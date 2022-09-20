8b5463f3f3f8b8ddf200858024cc480ff2787fb9
Pravin Prabhakaran <pravin.prabha123248@gmail.com>
2022-09-20 14:38:54 +0100

    react basic template complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Navbar, Nav } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="mainContainer">
       <header>
-
       </header>
+      <Navbar className="navStyle" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
     </div>
   );
 }
