cf1da6d768be50341daa15d601dd7fe8e7f227f6
Pravin Prabhakaran <78224090+prav123248@users.noreply.github.com>
2022-09-22 22:14:48 +0100

    navbar alignment change

--- frontend/src/components/Cnavbar.js
+++ frontend/src/components/Cnavbar.js
@@ -10,17 +10,18 @@ function Cnavbar(props) {
   return (
     <Navbar className="navStyle" variant="dark">
     <Navbar.Brand className="col-sm-1 text-center">Passage</Navbar.Brand>
-    <Container>
+    
+    <div className="navContainer">
       <Nav className="navBox">
-        <NavLink className="navLinkStyle" to="/Library" style={ { color: (props.navSet == 1) ? '#76B900' : '#777777' } }>Library</NavLink>
+        <NavLink className="navLinkStyle" to="/Library" style={ { color: (props.navSet == 1) ? '#76B900' : '#777777'} }>Library</NavLink>
         <NavLink className="navLinkStyle" to="/ToRead" style={ { color: (props.navSet == 2) ? '#76B900' : '#777777' } }>To Read</NavLink>
         <NavLink className="navLinkStyle" to="/Statistics" style={ { color: (props.navSet == 3) ? '#76B900' : '#777777' } }>Statistics</NavLink>              
       </Nav>
 
-      <Nav className="navBox">
+      <Nav className="navBox rightNav">
         <NavLink className="navLinkStyle" to="/Manage" style={ { color: (props.navSet == 4) ? '#76B900' : '#777777' } }>Manage Books</NavLink>
       </Nav>
-    </Container>
+    </div>
   </Navbar>
   );
 }
